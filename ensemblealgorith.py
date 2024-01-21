a90c40044327d6879fc4187e862979b61ba274e7
anilatambharii <41884022+anilatambharii@users.noreply.github.com>
2024-01-20 16:36:54 -0800

    Add files via upload

---
+++ ensemblealgorith.py
@@ -0,0 +1,77 @@
+#ensemble algorith
+# Import modules we'll need for this notebook
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.model_selection import train_test_split
+import numpy as np
+import matplotlib.pyplot as plt
+import requests
+from sklearn.linear_model import Lasso
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.tree import export_text
+
+# load the training dataset
+
+url = "https://raw.githubusercontent.com/MicrosoftDocs/mslearn-introduction-to-machine-learning/main/Data/ml-basics/daily-bike-share.csv"
+
+bike_data = requests.get(url)
+
+if bike_data.status_code == 200:
+    with open("grades.csv", "wb") as f:
+        f.write(bike_data.content)
+    print("File downloaded successfully.")
+else:
+    print(f"Failed to download the file. Status code: {bike_data.status_code}")
+
+bike_data = pd.read_csv('daily-bike-share.csv', delimiter=',', header='infer')
+
+bike_data['day'] = pd.DatetimeIndex(bike_data['dteday']).day
+numeric_features = ['temp', 'atemp', 'hum', 'windspeed']
+categorical_features = ['season','mnth','holiday','weekday','workingday','weathersit', 'day']
+bike_data[numeric_features + ['rentals']].describe()
+print(bike_data.head())
+
+
+# Separate features and labels
+# After separating the dataset, we now have numpy arrays named **X** containing the features, and **y** containing the labels.
+X, y = bike_data[['season','mnth', 'holiday','weekday','workingday','weathersit','temp', 'atemp', 'hum', 'windspeed']].values, bike_data['rentals'].values
+
+# Split data 70%-30% into training set and test set
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=0)
+
+print ('Training Set: %d rows\nTest Set: %d rows' % (X_train.shape[0], X_test.shape[0]))
+
+'''
+Now we have the following four datasets:
+
+X_train: The feature values we'll use to train the model
+y_train: The corresponding labels we'll use to train the model
+X_test: The feature values we'll use to validate the model
+y_test: The corresponding labels we'll use to validate the model'''
+
+from sklearn.ensemble import RandomForestRegressor
+
+# Train the model
+model = RandomForestRegressor().fit(X_train, y_train)
+print (model, "\n")
+
+# Evaluate the model using the test data
+predictions = model.predict(X_test)
+mse = mean_squared_error(y_test, predictions)
+print("MSE:", mse)
+rmse = np.sqrt(mse)
+print("RMSE:", rmse)
+r2 = r2_score(y_test, predictions)
+print("R2:", r2)
+
+# Plot predicted vs actual
+plt.scatter(y_test, predictions)
+plt.xlabel('Actual Labels')
+plt.ylabel('Predicted Labels')
+plt.title('Daily Bike Share Predictions')
+# overlay the regression line
+z = np.polyfit(y_test, predictions, 1)
+p = np.poly1d(z)
+plt.plot(y_test,p(y_test), color='magenta')
+plt.show()
